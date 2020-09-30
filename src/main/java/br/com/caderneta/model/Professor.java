0be8959a8e44c3dcecf5218a4f86191c2864e225
Paulo Silva <pharaujo14@gmail.com>
2020-09-30 19:28:19 -0300

    Criada classe professor, ajustes nas dependencias do Maven

---
+++ src/main/java/br/com/caderneta/model/Professor.java
@@ -0,0 +1,111 @@
+package br.com.caderneta.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "professores")
+public class Professor {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(length = 50, nullable = false)
+	private String nome;
+	
+	@Column(length = 150, nullable = false)
+	private String sobrenome;
+	
+	@Column(length = 11, nullable = false)
+	private String cpf;
+	
+	@Column(nullable = false)
+	private Date dataNascimento;
+	
+	public Professor() {}
+
+	public Professor(Long id, String nome, String sobrenome, String cpf, Date dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Professor other = (Professor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+
+}
