48e9872a14692bc8e62cea0151c8d1db0e3bc488
Paulo Silva <pharaujo14@gmail.com>
2020-10-19 22:42:44 -0300

    Ajustes no relacionamento aluno-turma

--- src/main/java/br/com/caderneta/model/Aluno.java
+++ src/main/java/br/com/caderneta/model/Aluno.java
@@ -1,12 +1,32 @@
 package br.com.caderneta.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "alunos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Aluno extends Pessoa{
-
+	
+	@ManyToMany(mappedBy = "alunos")
+	@JsonIgnore
+	private Set<Turma> turmas;
 
 
 }
